a878d5b9adeae03fba43ed25cc26bab424075182
TopHatPaladin <63761914+TopHatPaladin@users.noreply.github.com>
2025-08-27 22:38:16 -0400

    Update UBRemote.lua
    reorganizing BuildingRemoteGreatPeoplePoints to cut DB calls

--- Civs/[BNW] Agla's Green Ukraine (v 1)/Lua/UBRemote.lua
+++ Civs/[BNW] Agla's Green Ukraine (v 1)/Lua/UBRemote.lua
@@ -13,35 +13,47 @@ Events.SequenceGameInitComplete.Add(function() MapModData.BuildingRemoteGreatPeo
 --=======================================================================================================================
 -- Main Code
 --=======================================================================================================================
+local specWriter, specArtist, specMusician = GameInfo.Specialists.SPECIALIST_WRITER.ID, GameInfo.Specialists.SPECIALIST_ARTIST.ID, GameInfo.Specialists.SPECIALIST_MUSICIAN.ID
+local specScientist, specMerchant, specEngineer = GameInfo.Specialists.SPECIALIST_SCIENTIST.ID, GameInfo.Specialists.SPECIALIST_MERCHANT.ID, GameInfo.Specialists.SPECIALIST_ENGINEER.ID
+
+local tSpecGuilds = {}
+tSpecGuilds[specWriter] = GameInfo.Buildings.BUILDING_WRITERS_GUILD.ID
+tSpecGuilds[specArtist] = GameInfo.Buildings.BUILDING_ARTISTS_GUILD.ID
+tSpecGuilds[specMusician] = GameInfo.Buildings.BUILDING_MUSICIANS_GUILD.ID
+
+local tRemoteGreatPeoplePoints = {}
+for row in DB.Query("SELECT * FROM Building_RemoteGreatPeoplePoints") do
+	local iBuilding = GameInfoTypes[row.BuildingType]
+	tRemoteGreatPeoplePoints[iBuilding] = {}
+	local iSpec = GameInfoTypes[row.SpecialistType]
+	tRemoteGreatPeoplePoints[iBuilding]["Spec"] = iSpec
+	tRemoteGreatPeoplePoints[iBuilding]["Rate"] = row.RateChange
+	tRemoteGreatPeoplePoints[iBuilding]["Modified"] = row.Modified
+	tRemoteGreatPeoplePoints[iBuilding]["Capital"] = row.Capital
+end
+
 function BuildingRemoteGreatPeoplePoints(iPlayer)
 	local pPlayer = Players[iPlayer]
 	if pPlayer:IsBarbarian() or pPlayer:IsMinorCiv() then
 		return
 	end
 	
-	for row in GameInfo.Building_RemoteGreatPeoplePoints() do
+	for iBuilding, tSubtable in pairs(tRemoteGreatPeoplePoints) do
 	
 		local pTargetCity = pPlayer:GetCapitalCity()
 		if not(pTargetCity) then return end
 		-- Retrieve Info from tables
-		local iBuilding = GameInfo.Buildings[row.BuildingType].ID
-		local iType = GameInfo.Specialists[row.SpecialistType].ID
-		local iPoints = row.RateChange
-		local bModified = row.Modified
-		local bCapital = row.Capital
+		--local iBuilding = GameInfo.Buildings[row.BuildingType].ID
+		local iType = tSubtable["Spec"]
+		local iPoints = tSubtable["Rate"]
+		local iModified = tSubtable["Modified"]
+		local iCapital = tSubtable["Capital"]
 		
 		local iProgress = pTargetCity:GetSpecialistGreatPersonProgress(iType)
 		local iNumCities = 0
 		local iMod = 100
 		
-		iGuild = nil
-		if iType == GameInfo.Specialists.SPECIALIST_WRITER.ID then
-			iGuild = GameInfo.Buildings.BUILDING_WRITERS_GUILD.ID
-		elseif iType == GameInfo.Specialists.SPECIALIST_ARTIST.ID then
-			iGuild = GameInfo.Buildings.BUILDING_ARTISTS_GUILD.ID
-		elseif iType == GameInfo.Specialists.SPECIALIST_MUSICIAN.ID then
-			iGuild = GameInfo.Buildings.BUILDING_MUSICIANS_GUILD.ID
-		end
+		local iGuild = tSpecGuilds[iType]
 
 		for pCity in pPlayer:Cities() do
 			--if Guilded GP, identify city with Guild
@@ -50,7 +62,7 @@ function BuildingRemoteGreatPeoplePoints(iPlayer)
 					pTargetCity = pCity
 				end
 			-- Else find city with greatest progress
-			elseif not(bCapital) then
+			elseif (bCapital == 0) then
 				if iProgress < pCity:GetSpecialistGreatPersonProgress(iType) then
 					iProgress = pCity:GetSpecialistGreatPersonProgress(iType)
 					pTargetCity = pCity
@@ -64,7 +76,7 @@ function BuildingRemoteGreatPeoplePoints(iPlayer)
 		end
 		
 		if (pTargetCity ~= nil) and (iNumCities > 0) then
-			if (bModified) then
+			if (iModified == 1) then
 				iMod = iMod + (GetGreatPersonModifier(pPlayer, pTargetCity, iType))
 			end
 			iDelta = (iPoints * iNumCities * iMod)
@@ -93,7 +105,7 @@ function GetGreatPersonModifier(pPlayer, pTargetCity, iType)
 	local bGoldenAge = (pPlayer:GetGoldenAgeTurns() > 0)
 	
 	-- GP mods by type	
-	if (iType == GameInfo.Specialists.SPECIALIST_WRITER.ID) then
+	if (iType == specWriter) then
 		--print("is " .. GameInfo.Specialists[iType].Description)
 		iPlayerMod = iPlayerMod + pPlayer:GetGreatWriterRateModifier()
 		iPolicyMod = iPolicyMod + pPlayer:GetPolicyGreatWriterRateModifier()
@@ -103,7 +115,7 @@ function GetGreatPersonModifier(pPlayer, pTargetCity, iType)
 		if (bGoldenAge and pPlayer:GetGoldenAgeGreatWriterRateModifier() > 0) then
 		iGoldenAgeMod = iGoldenAgeMod + pPlayer:GetGoldenAgeGreatWriterRateModifier()
 		end
-	elseif (iType == GameInfo.Specialists.SPECIALIST_ARTIST.ID) then
+	elseif (iType == specArtist) then
 		--print("is " .. GameInfo.Specialists[iType].Description)
 		iPlayerMod = iPlayerMod + pPlayer:GetGreatArtistRateModifier()
 		iPolicyMod = iPolicyMod + pPlayer:GetPolicyGreatArtistRateModifier()
@@ -113,7 +125,7 @@ function GetGreatPersonModifier(pPlayer, pTargetCity, iType)
 		if (bGoldenAge and pPlayer:GetGoldenAgeGreatArtistRateModifier() > 0) then
 		iGoldenAgeMod = iGoldenAgeMod + pPlayer:GetGoldenAgeGreatArtistRateModifier()
 		end
-	elseif (iType == GameInfo.Specialists.SPECIALIST_MUSICIAN.ID) then
+	elseif (iType == specMusician) then
 		--print("is " .. GameInfo.Specialists[iType].Description)
 		iPlayerMod = iPlayerMod + pPlayer:GetGreatMusicianRateModifier()
 		iPolicyMod = iPolicyMod + pPlayer:GetPolicyGreatMusicianRateModifier()
@@ -123,21 +135,21 @@ function GetGreatPersonModifier(pPlayer, pTargetCity, iType)
 		if (bGoldenAge and pPlayer:GetGoldenAgeGreatMusicianRateModifier() > 0) then
 		iGoldenAgeMod = iGoldenAgeMod + pPlayer:GetGoldenAgeGreatMusicianRateModifier()
 		end
-	elseif (iType == GameInfo.Specialists.SPECIALIST_SCIENTIST.ID) then
+	elseif (iType == specScientist) then
 		--print("is " .. GameInfo.Specialists[iType].Description)
 		iPlayerMod = iPlayerMod + pPlayer:GetGreatScientistRateModifier()
 		iPolicyMod = iPolicyMod + pPlayer:GetPolicyGreatScientistRateModifier()
 		if (pWorldCongress ~= nil and pWorldCongress:GetScienceyGreatPersonRateModifier() ~= 0) then
 		iWorldCongressMod = iWorldCongressMod + pWorldCongress:GetScienceyGreatPersonRateModifier()
 		end
-	elseif (iType == GameInfo.Specialists.SPECIALIST_MERCHANT.ID) then
+	elseif (iType == specMerchant) then
 		--print("is " .. GameInfo.Specialists[iType].Description)
 		iPlayerMod = iPlayerMod + pPlayer:GetGreatMerchantRateModifier()
 		iPolicyMod = iPolicyMod + pPlayer:GetPolicyGreatMerchantRateModifier()
 		if (pWorldCongress ~= nil and pWorldCongress:GetScienceyGreatPersonRateModifier() ~= 0) then
 		iWorldCongressMod = iWorldCongressMod + pWorldCongress:GetScienceyGreatPersonRateModifier()
 		end
-	elseif (iType == GameInfo.Specialists.SPECIALIST_ENGINEER.ID) then
+	elseif (iType == specEngineer) then
 		--print("is " .. GameInfo.Specialists[iType].Description)
 		iPlayerMod = iPlayerMod + pPlayer:GetGreatEngineerRateModifier()
 		iPolicyMod = iPolicyMod + pPlayer:GetPolicyGreatEngineerRateModifier()
